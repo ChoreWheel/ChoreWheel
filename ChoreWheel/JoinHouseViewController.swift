4c8146c8416e071fd242b810cde3a774ecf29c9e
Kelly Lin <lin400@umail.ucsb.edu>
2021-05-22 12:03:30 -0700

    done with onCreate, onJoin doesn't work

--- ChoreWheel/JoinHouseViewController.swift
+++ ChoreWheel/JoinHouseViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class JoinHouseViewController: UIViewController {
 
@@ -13,11 +14,43 @@ class JoinHouseViewController: UIViewController {
     //enter code to join the house
     //each house has unique house id
     @IBAction func onCreate(_ sender: Any) {
+        let house = PFObject(className:"House")
+        let users = [PFUser.current()]
+        
+        house["name"] = houseidTextField.text
+        house["housemates"] = users
+        house.saveInBackground { (succeeded, error)  in
+            if (succeeded) {
+                print("saved")
+            } else {
+                print("error saving")
+            }
+        }
+        
+        var currentUser = PFUser.current()
+        currentUser?["house"] = houseidTextField.text
         
     }
     
     @IBAction func onJoin(_ sender: Any) {
+        //find house id in db
+        let houseName = houseidTextField.text! as String
         
+        let query = PFQuery(className: "House")
+        query.whereKey("name", equalTo: houseName)
+        /*
+        query.findObjectsInBackground { (house: [PFObject]?, error: Error?) in
+            if let error = error {
+                // The request failed
+                print(error.localizedDescription)
+            } else {
+                // comments now contains the comments for myPost
+                print(house["name"])
+            }
+        }
+        */
+        
+        //update housemates field in db
     }
     
     override func viewDidLoad() {
