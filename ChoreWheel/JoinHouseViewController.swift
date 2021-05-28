c39fe2ab248524084970f6263a0735f7d02ce3ff
Kelly Lin <lin400@umail.ucsb.edu>
2021-05-28 10:50:16 -0700

    Update JoinHouseViewController.swift

--- ChoreWheel/JoinHouseViewController.swift
+++ ChoreWheel/JoinHouseViewController.swift
@@ -19,6 +19,7 @@ class JoinHouseViewController: UIViewController {
         
         house["name"] = houseidTextField.text
         house["housemates"] = users
+        house["uuid"] = UUID().uuidString
         house.saveInBackground { (succeeded, error)  in
             if (succeeded) {
                 print("saved")
@@ -33,22 +34,38 @@ class JoinHouseViewController: UIViewController {
     }
     
     @IBAction func onJoin(_ sender: Any) {
-        //find house id in db
-        let houseName = houseidTextField.text! as String
-        
+        //get id
+        let id = houseidTextField.text! as String
+        let fakeUser = PFUser()
+        fakeUser.username = "test1"
+        fakeUser.email = "test1@gmail.com"
+        fakeUser.password = "12345678"
         let query = PFQuery(className: "House")
-        query.whereKey("name", equalTo: houseName)
-        /*
+        query.includeKey("housemates")
+        query.whereKey("uuid", equalTo: id)
+        
+        //get the list of PFObjects with the id, take the first thing in the list
         query.findObjectsInBackground { (house: [PFObject]?, error: Error?) in
             if let error = error {
                 // The request failed
                 print(error.localizedDescription)
             } else {
+                
+                
+                guard let houses = house else {return}
+                let house = houses[0]
+               
+                var housemates = house["housemates"] as! [PFUser]
+                housemates.append(fakeUser)
+          
+                //save housemates to db
+                
+                
                 // comments now contains the comments for myPost
-                print(house["name"])
+                //print(house["name"])
             }
         }
-        */
+        
         
         //update housemates field in db
     }
